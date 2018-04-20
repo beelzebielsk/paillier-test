a872bb9edeb0bd190711e5750f78b80c8df3c1e7
adam <ibrahimadam193@gmail.com>
2018-04-19 23:03:11 -0400

    Implement all interpreter tests
    Changes to be committed:
          modified:   interpreter-test.cpp
    
    Changes:
    
    - testResult:
        - Changed testResult to use a list of pairs instead of a map,
          because mapping requires that a type is hashable, and I had no
          such guarantee on my types. Plus, my cases didn't use any of the
          actual map features. I only needed a pair list.
        - Added in a try/catch block to catch interpreter errors, so that
          the test doesn't fail when those exceptions happen.
    - Reorganized my preamble so that using statements for names were
      paired with the include directives from which the names came.
    - Changed some older tests to use testResults.
    - Moved some of the `operator<<` functions for interpreter types into
      the type definitions themselves.
    - Added some more general print functions for containers.

--- interpreter-test.cpp
+++ interpreter-test.cpp
@@ -1,39 +1,52 @@
 #include <interpreter/parser.h>
 #include <iostream>
+    using std::cout;
+    using std::endl;
+    using std::cerr;
 #include <sstream>
+    using std::stringstream;
 #include <vector>
+    using std::vector;
 
 #include <unordered_map>
+    using std::unordered_map;
+#include <utility>
+    using std::pair;
 
-using std::stringstream;
-using std::cout;
-using std::endl;
-using std::cerr;
-using std::unordered_map;
 using std::string;
+
+template <typename T1, typename T2>
+using pair_list = vector<pair<T1, T2>>;
 
 int testTokenizer();
 int testIsInstruction();
-int testInstructionType(); // Not working with testResults.
+int testInstructionType();
 int testIsDeclarationType();
-int testDeclarationType(); // TODO
-int testCreateInstructionList();
+int testDeclarationType();
+int testCreateInstructionList(); // TODO
 int testExecute(); // TODO
 
 std::ostream& operator<<(std::ostream&, Instruction::Type);
 
 template<typename Case, typename Result>
-int testResults(unordered_map<Case, Result> cases, auto test) {
+int testResults(pair_list<Case, Result> cases, auto test) {
     bool allCorrect = true;
     for (auto pair : cases) {
         Case thisCase = pair.first;
         Result expected = pair.second;
-        Result actual = test(thisCase);
-        if (actual != expected) {
-            cerr << "Case " << thisCase << " has incorrect result."
-                << endl
-                << "Expected: " << expected << endl
-                << "Result: " << actual << endl;
+        Result actual;
+        try {
+            actual = test(thisCase);
+            if (actual != expected) {
+                cerr << "Case " << thisCase 
+                    << " has incorrect result." << endl
+                    << "Expected: " << expected << endl
+                    << "Result: " << actual << endl;
+                allCorrect = false;
+            }
+        }
+        catch(InterpreterError& error) {
+            cout << error << endl;
             allCorrect = false;
         }
     }
@@ -52,106 +65,59 @@ int main() {
     TEST(IsInstruction)
     TEST(InstructionType)
     TEST(IsDeclarationType)
+    TEST(DeclarationType)
+    TEST(CreateInstructionList)
+    TEST(Execute)
 }
 
 int testTokenizer() {
-    stringstream file(R"(
-        add one two
-        mult three four
-        load five
-        output six
-    )");
-
-    vector<Token> expectedTokens = {
-        "add", "one", "two",
-        "mult", "three", "four",
-        "load", "five",
-        "output", "six",
+    pair_list<string, vector<Token>> cases = {
+        {
+            R"(
+                add one two
+                mult three four
+                load five
+                output six
+            )",
+            vector<Token> {
+                "add", "one", "two",
+                "mult", "three", "four",
+                "load", "five",
+                "output", "six"
+            }
+        }
     };
 
-    vector<Token> tokens = tokenizeProgram(file);
-    if (expectedTokens != tokens) {
-        cout << "Tokenizer failed!" << endl;
-        cout << "Tokens found:" << endl;
-        for (auto token : tokens) { cout << token << endl; }
-        cout << "Tokens expected:" << endl;
-        for (auto token : expectedTokens) { cout << token << endl; }
-        return false;
-    }
+    auto test = [](string program) {
+        // So that s can be passed by reference.
+        stringstream s = stringstream(program);
+        return tokenizeProgram(s);
+    };
 
-    return true;
-
+    return testResults(cases, test);
 }
 
 int testIsInstruction() {
-    string add = "add";
-    string mult = "mult";
-    string load = "load";
-    string output = "output";
+    pair_list<string, bool> cases = {
+        {"add", true},
+        {"mult", true},
+        {"load", true},
+        {"output", true}
+    };
 
-    bool isAdd = Instruction::isInstructionName(add);
-    bool isMult = Instruction::isInstructionName(mult);
-    bool isLoad = Instruction::isInstructionName(load);
-    bool isOutput = Instruction::isInstructionName(output);
+    auto test = [](string name) { 
+        return Instruction::isInstructionName(name);
+    };
 
-    if (!isAdd) {
-        cerr << "Add is NOT interpreted as instruction." << endl;
-    }
-    if (!isMult) {
-        cerr << "Mult is NOT interpreted as instruction." << endl;
-    }
-    if (!isLoad) {
-        cerr << "Load is NOT interpreted as instruction." << endl;
-    }
-    if (!isOutput) {
-        cerr << "Output is NOT interpreted as instruction." << endl;
-    }
-
-    return isAdd && isMult && isLoad && isOutput;
+    return testResults(cases, test);
 }
 
 int testInstructionType() {
-    /*
-    string add = "add";
-    string mult = "mult";
-    string load = "load";
-    string output = "output";
-
-    Instruction::Type typeAdd = Instruction::which(add);
-    Instruction::Type typeMult = Instruction::which(mult);
-    Instruction::Type typeLoad = Instruction::which(load);
-    Instruction::Type typeOutput = Instruction::which(output);
-
-    bool addCorrect = typeAdd == Instruction::add;
-    bool multCorrect = typeMult == Instruction::mult;
-    bool loadCorrect = typeLoad == Instruction::load;
-    bool outputCorrect = typeOutput == Instruction::output;
-
-    if (!addCorrect) {
-        cerr << "add is interpreted incorrectly as " << typeAdd
-            << endl;
-    }
-    if (!multCorrect) {
-        cerr << "mult is interpreted incorrectly as " << typeMult
-            << endl;
-    }
-    if (!loadCorrect) {
-        cerr << "load is interpreted incorrectly as " << typeLoad
-            << endl;
-    }
-    if (!outputCorrect) {
-        cerr << "output is interpreted incorrectly as " << typeOutput
-            << endl;
-    }
-
-    return addCorrect && multCorrect && loadCorrect && outputCorrect;
-    */
-
-    unordered_map<string, Instruction::Type> cases = {
-        {"add", Instruction::add},
-        {"mult", Instruction::mult},
-        {"load", Instruction::load},
-        {"output", Instruction::output}
+    pair_list<string, Instruction::Type> cases = {
+        {"add", Instruction::Type::add},
+        {"mult", Instruction::Type::mult},
+        {"load", Instruction::Type::load},
+        {"output", Instruction::Type::output}
     };
 
     auto test = [](string Case) {
@@ -159,12 +125,10 @@ int testInstructionType() {
     };
 
     return testResults(cases, test);
-    //Instruction::Type type = Instruction::add;
-    //return true;
 }
 
 int testIsDeclarationType() {
-    unordered_map<string, bool> cases = {
+    pair_list<string, bool> cases = {
         {"input", true},
         {"share", true}
     };
@@ -177,24 +141,90 @@ int testIsDeclarationType() {
 }
 
 int testDeclarationType() {
+    pair_list<string, Declaration::Type> cases = {
+        {"input", Declaration::Type::input},
+        {"share", Declaration::Type::share}
+    };
+
+    auto test = [](string type) { 
+        return Declaration::which(type);
+    };
+
+    return testResults(cases, test);
 }
 
 int testCreateInstructionList() {
+    // /*
+    pair_list<string, vector<Instruction>> cases = {
+        {
+            R"(
+                add one two three
+                mult three four five
+                load six seven
+                output eight nine
+            )",
+            {
+                Instruction("add", "one", "two", "three"),
+                Instruction("mult", "three", "four", "five"),
+                Instruction("load", "six", "seven"),
+                Instruction("output", "eight", "nine")
+            }
+        }
+    };
+
+    auto test = [](string program) { 
+        stringstream s = stringstream(program);
+        return createInstructionList(tokenizeProgram(s));
+    };
+
+    return testResults(cases, test);
 }
 
 int testExecute() {
+    pair_list< pair<string, ProgramState>, ProgramState > cases = {
+        {
+            {
+                "add dest two three",
+                { {"two", 2}, {"three", 3} }
+            },
+            { {"dest", 5}, {"two", 2}, {"three", 3} }
+        }
+    };
+
+    auto test = [](pair<string, ProgramState> program) {
+        stringstream s(program.first);
+        vector<Token> t = tokenizeProgram(s);
+        vector<Instruction> i = createInstructionList(t);
+        return execute(i, program.second);
+    };
+
+    return testResults(cases, test);
 }
 
-std::ostream& operator<<(std::ostream& out, Instruction::Type type) {
-    if (type == Instruction::add) {
-        out << "Instruction::add";
-    } else if (type == Instruction::mult) {
-        out << "Instruction::mult";
-    } else if (type == Instruction::load) {
-        out << "Instruction::load";
-    } else if (type == Instruction::output) {
-        out << "Instruction::output";
+template <typename T>
+std::ostream& operator<<(std::ostream& out, vector<T> ts) {
+    out << "(";
+    for (T t : ts) {
+        out << t << ", " ;
     }
+    out << ")";
+    return out;
+}
 
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out,
+        unordered_map<T1, T2> map) {
+    out << "{";
+    for (auto pair : map) {
+        out << "'" << pair.first << "' : '" << pair.second << "', ";
+    }
+    out << "}";
+    return out;
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, pair<T1, T2> p) {
+    out << "("
+        << "'" << p.first << "', '" << p.second << "')";
     return out;
 }
